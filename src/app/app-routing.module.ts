2ea10020968513c10bd03f2fe90591c0c3d9ac91
Rafael Henrique <rafaelh.silva@amazoninf.com.br>
2023-10-19 22:32:41 -0300

    Configuração inicial do projeto FADESP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TarefaListaComponentComponent } from './components/tarefa-lista-component/tarefa-lista-component.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/tarefas', pathMatch: 'full' },
+  { path: 'tarefas', component: TarefaListaComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarefaListaComponentComponent } from './components/tarefa-lista-component/tarefa-lista-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarefaListaComponentComponent
   ],
   imports: [
     BrowserModule,
