b24d78aa6b7b535b2356f4dd4c14e9868c5ce7b9
Rafael Henrique <rafaelh.silva@amazoninf.com.br>
2023-10-21 00:05:29 -0300

    Criação da lógica e estilização inicial do HTML e CSS do componente "tarefa-lista-component"

--- src/app/components/tarefa-lista-component/tarefa-lista-component.component.ts
+++ src/app/components/tarefa-lista-component/tarefa-lista-component.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Task } from '../../models/tarefa.model';
+import { TarefaService } from '../../services/tarefa-service.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-tarefa-lista-component',
@@ -6,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarefa-lista-component.component.css']
 })
 export class TarefaListaComponentComponent implements OnInit {
+  displayedColumns: string[] = ['isDone', 'name', 'cpf', 'responsible', 'deadline', 'status','deleteTask'];
+  dataSource!: MatTableDataSource<Task>;
 
-  constructor() { }
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  constructor(
+    private tarefaService: TarefaService
+  ) { }
 
   ngOnInit(): void {
+    
+    this.loadData();
+  }
+  private loadData() {
+    this.tarefaService.getTasks()
+      .subscribe(tasks => {
+        if (tasks) {
+          this.dataSource = new MatTableDataSource(tasks);
+          this.dataSource.paginator = this.paginator;
+        }
+    })
+  }
+  itemActionDeleteClick(taskId: string) {
+    this.tarefaService.deleteTask(taskId);
+    this.loadData();
+  }
+  onCheckboxChange(event: MatCheckboxChange, task: Task): void {
+    task.isDone = event.checked;
+    this.tarefaService.updateTask(task).subscribe(updatedTask => {
+      console.log(updatedTask)
+    });
   }
 
+
+  public getStatusClass(status: string) {
+    const classList = {
+      'finish': status === 'Concluído',
+      'open': status === 'Aberta',
+      'expired': status === 'Expirada',
+    };
+    return classList;
+  }
 }
