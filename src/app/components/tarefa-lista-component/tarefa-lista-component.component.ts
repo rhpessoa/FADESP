cd4ab8fdd50968bb3b25352e197c39c105303ba8
Rafael Henrique <rafaelh.silva@amazoninf.com.br>
2023-10-21 12:28:20 -0300

    Adicionado o filtro de tarefas na aplicação além da implementação da sua lógica.

--- src/app/components/tarefa-lista-component/tarefa-lista-component.component.ts
+++ src/app/components/tarefa-lista-component/tarefa-lista-component.component.ts
@@ -4,6 +4,8 @@ import { Task } from '../../models/tarefa.model';
 import { TarefaService } from '../../services/tarefa-service.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatCheckboxChange } from '@angular/material/checkbox';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-tarefa-lista-component',
@@ -11,39 +13,49 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
   styleUrls: ['./tarefa-lista-component.component.css']
 })
 export class TarefaListaComponentComponent implements OnInit {
-  displayedColumns: string[] = ['isDone', 'name', 'cpf', 'responsible', 'deadline', 'status','deleteTask'];
+  displayedColumns: string[] = ['isDone', 'name', 'cpf', 'responsible', 'deadline', 'status', 'deleteTask'];
   dataSource!: MatTableDataSource<Task>;
+  noDataFound: boolean = true;
+  form!: FormGroup;
+  statusOptions: string[] = ['Concluído', 'Aberta', 'Expirada'];
 
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
 
   constructor(
-    private tarefaService: TarefaService
-  ) { }
+    private tarefaService: TarefaService,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      status: ['']
+    });
+  }
 
   ngOnInit(): void {
-    
     this.loadData();
   }
+
   private loadData() {
     this.tarefaService.getTasks()
       .subscribe(tasks => {
         if (tasks) {
+          this.noDataFound = (tasks === null || tasks.length === 0);
           this.dataSource = new MatTableDataSource(tasks);
           this.dataSource.paginator = this.paginator;
         }
-    })
+      })
   }
+
   itemActionDeleteClick(taskId: string) {
     this.tarefaService.deleteTask(taskId);
     this.loadData();
   }
+
   onCheckboxChange(event: MatCheckboxChange, task: Task): void {
     task.isDone = event.checked;
     this.tarefaService.updateTask(task).subscribe(updatedTask => {
       console.log(updatedTask)
     });
   }
-
 
   public getStatusClass(status: string) {
     const classList = {
@@ -52,5 +64,9 @@ export class TarefaListaComponentComponent implements OnInit {
       'expired': status === 'Expirada',
     };
     return classList;
+  }
+
+  onStatusChange(event: MatSelectChange): void {
+    this.dataSource.filter = (event.value);
   }
 }
